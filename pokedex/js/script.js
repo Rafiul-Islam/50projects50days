0f1e2a59953066f4be8f1a4a98425cd247a909b3
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-01-13 20:14:24 +0600

    Pokedex - added with updated readme file.

---
+++ pokedex/js/script.js
@@ -0,0 +1,60 @@
+const pokeContainer = document.getElementById("pokeContainer");
+const pokemon_count = 30;
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  createPokemonCard(data);
+};
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document
+    .querySelector("[pokemon]")
+    .content.children[0].cloneNode(true);
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, "0");
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  pokemonEl.querySelector(
+    "img"
+  ).src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`;
+
+  pokemonEl.querySelector(".number").textContent = `#${id}`;
+  pokemonEl.querySelector(".name").textContent = name;
+  pokemonEl.querySelector(".type").textContent = type;
+
+  pokeContainer.appendChild(pokemonEl);
+};
+
+fetchPokemons();
