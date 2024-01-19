832f029c2c57fc92e751ea6841f1597022f608a0
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-01-19 16:28:48 +0600

    3d Background Boxes - added with updated readme file.

---
+++ 3d-background-boxes/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const boxes = document.querySelector(".boxes");
+  const button = document.querySelector("#magicButton");
+
+  (function createBox() {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.backgroundPosition = `${-125 * j}px ${-125 * i}px`;
+        boxes.appendChild(box);
+      }
+    }
+  })();
+
+  button.addEventListener("click", function () {
+    boxes.classList.toggle("big");
+  });
+});
